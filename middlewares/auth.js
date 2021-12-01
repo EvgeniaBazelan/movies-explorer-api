156674a9dde6c765ce374e6ca4b473d566c6b2a4
Evgenia Bazelan <evgeniabazelan@gmail.com>
2021-12-01 18:52:33 +0300

    jwt secret

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -24,7 +24,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return res
       .status(401)
